09685c66024e625a02a38044122b6bbf89c0e020
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-07-08 16:19:18 +0530

    Create checker.h

---
+++ checker.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#define MINIMUM_SOC    20.0
+#define MAXIMUM_SOC    80.0
+
+#define MINIMUM_CHARGERATE    0
+#define MAXIMUM_CHARGERATE    0.8
+
+#define MINIMUM_TEMPERATURE    0.0
+#define MAXIMUM_TEMPERATURE    45.0
+
