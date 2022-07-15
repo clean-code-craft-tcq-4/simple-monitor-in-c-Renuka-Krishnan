57ba232fd3f689abd1808b424305e9ac8bb7bdb2
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-07-15 15:02:27 +0530

    Create BatteryCheck.c

---
+++ BatteryCheck.c
@@ -0,0 +1,58 @@
+#include <checker.h>
+
+int CheckBatteryStatus(float ActualValue, float minimum, float maximum)
+{
+  if (ActualValue < minimum || ActualValue > maximum)
+  {
+  return Check_NOT_OK;
+  } 
+else
+  {
+  return Check_OK;
+  }
+  
+}
+
+
+int TemperatureCheck(float temp)
+{
+int val = CheckBatteryStatus(temperature, 0, 45);
+if(val == Check_NOT_OK)
+{
+printf("Temperature value not in range!\n");
+return Check_NOT_OK;
+}
+else 
+{
+return Check_OK;
+} 
+} 
+
+int SocCheck(float soc)
+{
+    int val = CheckBatteryStatus(soc, 20, 80);
+    if(val == Check_NOT_OK)
+    {
+         printf("State of Charge out of range!\n");
+        return Check_NOT_OK;
+    }
+    else 
+    {
+         return Check_OK;
+    }      
+}
+
+int ChargeRateCheck(float chargeRate)
+{
+   int val = CheckBatteryStatus(chargeRate, 0, 0.8);
+    if(val == Check_NOT_OK)
+    {
+        printf("State of Charge out of range!\n");
+        return Check_NOT_OK;
+    }
+    else 
+    {
+         return Check_OK;
+    }      
+}
+
