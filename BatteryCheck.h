260bee88e682a4fcd90efbf9ad18879bc1bf1821
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-09-01 14:26:57 +0530

    Update BatteryCheck.h

--- BatteryCheck.h
+++ BatteryCheck.h
@@ -1,7 +1,45 @@
-#define Check_OK 1
-#define Check_NOT_OK 0 
+#include <stdbool.h>
 
-int SocCheck(float soc);
-int ChargeRateCheck(float chargeRate);
-int TemperatureCheck(float temp);
-int CheckBatteryStatus(float ActualValue, float minimum, float maximum);
+#define Check_OK                       0
+#define Check_NOT_OK                   1
+
+#define TEMP_UNIT_CELCIUS         'C'
+#define TEMP_UNIT_FARENHEIT       'F'
+
+#define MINIMUM_LOWTEMPBREACH        0
+#define MINIMUM_LOWTEMPWARNING       5
+#define MINIMUM_TEMPNORMAL           10
+#define MINIMUM_HIGHTEMPWARNING      40
+#define MINIMUM_HIGHTEMPBREACH       45
+#define MIXIMUM_HIGHTEMPBREACH       80
+
+#define MINIMUM_LOWSOCBREACH          0
+#define MINIMUM_LOWSOCWARNING        20
+#define MINIMUM_SOCNORMAL		         24
+#define MINIMUM_HIGHSOCWARNING 	     75
+#define MINIMUM_HIGHSOCBREACH        80
+#define MIXIMUM_HIGHOCBREACH         100
+
+
+#define MINIMUM_LOWCHARGERATEBREACH   0
+#define MINIMUM_LOWCHARGERATEWARNING  0.2
+#define MINIMUM_CHARGERATENORMAL      0.3
+#define MINIMUM_HIGHCHARGERATEWARNING 0.7
+#define MINIMUM_HIGHCHARGERATEBREACH  0.8
+#define MIXIMUM_HIGHCHARGERATEBREACH  1.0
+
+extern int BatteryStatusOK( float temperature, float soc , float ChargeRate, char TemperatureUnit);
+extern int BatteryStatusNotOK ( float temperature, float soc , float ChargeRate,char TemperatureUnit);
+
+extern int BatterySocLowLimit(float soc);
+extern int BatterySocHighLimit(float soc);
+extern int checkSocOk(float soc );
+extern int BatteryTempLowLimit(float temperature);
+extern int BatteryTempHighLimit(float temperature , char TemperatureUnit);
+extern int checkTempOK(float temp , char TemperatureUnit );
+extern int BatteryChargeRateLowLimit(float chargeRate);
+extern int BatteryChargeRateHighLimit(float chargeRate);
+extern int checkchargeRate(float chargeRate );
+extern bool BatteryTempUnit(char cel);
+extern float BatteryConvertTemp(float temperature, char TemperatureUnit);
+
