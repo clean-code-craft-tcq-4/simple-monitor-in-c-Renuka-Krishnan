14260ddbc52907c36b4ff61185b266bf92e0a36e
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-09-01 14:03:13 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -2,12 +2,5 @@
 #include <assert.h>
 #include <stdbool.h>
 
-#define MINIMUM_SOC    20.0
-#define MAXIMUM_SOC    80.0
-
-#define MINIMUM_CHARGERATE    0
-#define MAXIMUM_CHARGERATE    0.8
-
-#define MINIMUM_TEMPERATURE    0.0
-#define MAXIMUM_TEMPERATURE    45.0
-
+extern int batteryIsOk(float temperature, float soc, float ChargeRate, char TemperatureUnit);
+extern int BatteryIsNotOk(float temperature, float soc , float ChargeRate, char TemperatureUnit);
