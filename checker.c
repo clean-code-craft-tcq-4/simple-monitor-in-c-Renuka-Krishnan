2949a5ea68bbe5acc4400e266b5b4f59b382a9a8
Renuka-Krishnan <105350454+Renuka-Krishnan@users.noreply.github.com>
2022-09-01 13:36:16 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,19 +1,24 @@
 #include "checker.h"
 #include "BatteryChecker.h"
 
-int batteryIsOk(float temperature, float soc, float chargeRate) 
+int batteryIsOk(float temperature, float soc, float ChargeRate, char TemperatureUnit) 
 {
-int  TempResult = TemperatureCheck (temperature);
-int SocResult = SocCheck(soc);
-int chargeRateResult = ChargeRateCheck(chargeRate); 
-return (TempResult && SocResult && chargeRateResult);
-
-
+int Result = BatteryStatusOK(temperature,soc,ChargeRate,TemperatureUnit); 
+return Result;
 }
+
+ int BatteryIsNotOk(float temperature, float soc , float ChargeRate, char TemperatureUnit)
+{	
+	int Result = BatteryStatusNotOK(temperature,soc,ChargeRate,TemperatureUnit);
+	return Result;
+} 
 
 int main() 
-{
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
-  assert(!batteryIsOk(-1, 100, 0.9));
-}
+  
+  {
+    assert(!batteryIsOk(-1, 100, 0.9));
+    assert(BatteryIsOk(25,37,0.7,'C'));
+    assert(BatteryIsOk(39,280,0.6,'F'));
+    assert(BatteryIsNotOk(78,-1,1.0,'N')); 
+	  assert(BatteryIsNotOk(89,48,0.9,'C')); 
+ }
